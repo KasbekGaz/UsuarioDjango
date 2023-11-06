ce2753dbd64c6cdc3875b08e2e965f82c0d74b5b
KasbekGaz <oswa.laza.flores@hotmail.com>
2023-11-06 12:09:32 -0600

    rutas para User

--- UAPI/urls.py
+++ UAPI/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Obra.views import CreateUserView, UserLoginView, UserLogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', CreateUserView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
 ]
