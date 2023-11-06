6c01549519355c6cca7deedf61985efc8509cf7f
KasbekGaz <oswa.laza.flores@hotmail.com>
2023-11-06 12:08:36 -0600

    User vistas registro login logout

--- Obra/views.py
+++ Obra/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import CustomUser
+from .serializers import CustomUserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+#! vistas
+class CreateUserView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = (permissions.AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'message': 'Usuario registrado con éxito', 'user_id': user.id, 'token': token.key}, status=status.HTTP_201_CREATED)
+
+
+class UserLoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+
+        if created:
+            message = "Inicio de sesión exitoso."
+        else:
+            message = "Sesión ya iniciada para este usuario."
+
+        return Response({'token': token.key, 'message': message}, status=status.HTTP_200_OK)
+
+
+class UserLogoutView(generics.DestroyAPIView):
+    queryset = Token.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def destroy(self, request, *args, **kwargs):
+        user = request.user
+        tokens = Token.objects.filter(user=user)
+        tokens.delete()
+        return Response({'message': 'Cierre de sesión exitoso'}, status=status.HTTP_204_NO_CONTENT)
