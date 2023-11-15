f25efe6042ac303aa015f0861c0e01b9bc3edb54
KasbekGaz <oswa.laza.flores@hotmail.com>
2023-11-15 16:44:42 -0600

    Config back para conectar al front

--- UAPI/settings.py
+++ UAPI/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'Obra',
@@ -53,6 +54,8 @@ AUTH_USER_MODEL = 'Obra.CustomUser'  # ponemos la app y modelo ucstomeruser
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    # des CORS para ocmunicar con frontend
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -132,3 +135,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    # "http://localhost:3000",  # O el origen de tu frontend
+]
