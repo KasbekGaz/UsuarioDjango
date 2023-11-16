c52d140d99ee4f6b425035fb7033564f195ed929
KasbekGaz <oswa.laza.flores@hotmail.com>
2023-11-15 18:11:49 -0600

    Config front-end listo para modificar

--- UAPI/settings.py
+++ UAPI/settings.py
@@ -137,5 +137,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    # "http://localhost:3000",  # O el origen de tu frontend
+    "http://localhost:5173",  # O el origen de tu frontend
 ]
