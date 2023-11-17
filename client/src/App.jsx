d62a5b3a41f3ef941049226a2f92f97cd0abfe1f
KasbekGaz <oswa.laza.flores@hotmail.com>
2023-11-17 00:33:43 -0600

    Estableciendo rutas usuario

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import RegisterView from "./pages/RegisterView";
+import { LoginView } from "./pages/LoginView"
 
 function App() {
   return(
-    <div className='App'>
-      <RegisterView />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to ="/login"/>} />
+        <Route path="/register" element={<RegisterView/>} />
+        <Route path="/login" element={<LoginView/>} />
+      </Routes>
+    </BrowserRouter>
+
+
   );
 }
 
