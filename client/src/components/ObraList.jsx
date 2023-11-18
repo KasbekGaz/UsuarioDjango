533eafeb211d9c8bbb82cd732e770fb2b822e8fb
KasbekGaz <oswa.laza.flores@hotmail.com>
2023-11-18 14:58:56 -0600

    Probando Registro Obras

--- client/src/components/ObraList.jsx
+++ client/src/components/ObraList.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ObrasList = () => {
+export function ObraList () {
   const [obras, setObras] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -21,7 +21,7 @@ const ObrasList = () => {
         setObras(response.data);
       } catch (error) {
         console.error('Error al obtener la lista de obras:', error.response);
-        setError('Error al cargar la lista de obras. Por favor, intenta de nuevo más tarde.');
+        setError('Error al cargar la lista de obras. Por favor, intenta de nuevo más tarde. Puede que no haya inicidado sesion');
       } finally {
         setLoading(false);
       }
@@ -39,18 +39,20 @@ const ObrasList = () => {
   }
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="bg-blue p-8 mx-10 my-10 shadow-md rounded-md text-white justify-right">
+      <div className="container mx-auto p-4">
       <h2 className="text-2xl font-semibold mb-4">Lista de Obras Registradas</h2>
       
       <ul className="list-disc pl-4">
         {obras.map((obra) => (
           <li key={obra.id} className="mb-2">
-            <strong>{obra.nombre}</strong> - {obra.localidad}, {obra.municipio} ({obra.dependencia}) - Fecha: {obra.fecha}
+            <strong>{obra.nombre}</strong> - {obra.localidad}, {obra.municipio} ({obra.dependencia}) - Fecha: {obra.fecha} - Presupuesto: {obra.p_inicial}
           </li>
         ))}
       </ul>
     </div>
+    </div>
   );
 };
 
-export default ObrasList;
+export default ObraList;
